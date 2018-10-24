1886d840f4a4af7a775a331e78454bc9ceee2ad1
bantu <76251098@qq.com>
2018-10-24 20:58:01 +0800

    2018-10-14

--- blog/views.py
+++ blog/views.py
@@ -23,3 +23,53 @@ def get_blog_list_common_data(request, blogs_all_list):
 		page_range.append('...')
 
 	# 加上首页和末页
+	if page_range[0] != 1:
+		page_range.insert(0,1)
+	if page_range[-1] != paginator.num_pages:
+		page_range.append(paginator.num_pages)
+
+	# 获取日期归档对应的博客数量
+	blog_dates = Blog.objects.dates('created_time', 'month', order='DESC')
+	blog_dates_dict = {}
+	for blog_date in blog_dates:
+		blog_count = Blog.objects.filter(created_time__year=blog_date.year,
+			created_time__month=blog_date.month).count() # 这里的__year和__month需要考察
+		blog_dates_dict[blog_date] = blog_count
+
+	context = {}
+	context['blogs'] = page_of_blogs.object_list
+	context['page_of_blogs'] = page_of_blogs
+	context['page_range'] = page_range
+	context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog')) # annotate需要考察
+	context['blog_dates'] = blog_dates_dict
+	return context
+
+	def blog_list(request):
+		blogs_all_list = Blog.objects.all()
+		context = get_blog_list_common_data(request, blogs_all_list)
+		return render(request, 'blog/blog_list.html', context)
+
+	def blogs_with_type(request, blog_type_pk):
+		blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+		blogs_all_list = Blog.objects.filter(blog_type=blog_type)
+		context = get_blog_list_common_data(request,blogs_all_list)
+		context['blog_type'] = blog_type
+		return render(request, 'blog/blogs_with_date.html', context)
+
+	def blog_with_date(request, year, month):
+		blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month) # 横线写法
+		context = get_blog_list_common_data(request, blog_all_list)
+		context['blogs_with_date'] = '%s年%s月'%(year, month)
+		return render(request, 'blog/blogs_with_date.html', context)
+
+	def blog_detail(request, blog_pk):
+		blog = get_object_or_404(Blog, pk=blog_pk)
+		read_cookie_key = read_statistics_once_read(request, blog)
+
+		context = {}
+		context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
+		context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
+		context['blog'] = blog
+		response = render(request, 'blog/blog_detail.html', context)
+		response.set_cookie(read_cookie_key, 'true') # set_cookie写法
+		return response
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('comment/', include('commment.urls')),
