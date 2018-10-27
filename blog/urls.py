587fff8a5435c158b915bca186649ccf4be26a26
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-10-28 01:20:38 +0800

    2018-10-28

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ import django.urls import auth
 from . import views
 
 urlpatterns = [
-	path('',views.blog_list)
+#地址为：localhost/blog/
+	path('',views.blog_list, name='blog_list'), # 默认进入后显示博客列表
+	path('<int:blog_pk>', views.blog_detail, name="blog_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from read_statistics.utils import read_statistics_once_read
 
 # Create your views here.
 def get_blog_list_common_data(request, blogs_all_list):
+	'''处理拿到的博客数据，处理页码'''
 	paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)
 	page_num = request.GET.get('page', 1) # 获取url的页面参数（GET请求）
 	page_of_blogs = paginator.get_page(page_num)
@@ -44,32 +45,35 @@ def get_blog_list_common_data(request, blogs_all_list):
 	context['blog_dates'] = blog_dates_dict
 	return context
 
-	def blog_list(request):
-		blogs_all_list = Blog.objects.all()
-		context = get_blog_list_common_data(request, blogs_all_list)
-		return render(request, 'blog/blog_list.html', context)
+def blog_list(request):
+	'''拿到所有的博客列表'''
+	blogs_all_list = Blog.objects.all()
+	context = get_blog_list_common_data(request, blogs_all_list)
+	return render(request, 'blog/blog_list.html', context)
 
-	def blogs_with_type(request, blog_type_pk):
-		blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-		blogs_all_list = Blog.objects.filter(blog_type=blog_type)
-		context = get_blog_list_common_data(request,blogs_all_list)
-		context['blog_type'] = blog_type
-		return render(request, 'blog/blogs_with_date.html', context)
+def blogs_with_type(request, blog_type_pk):
+	'''拿到某种类型的博客列表'''
+	blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+	blogs_all_list = Blog.objects.filter(blog_type=blog_type)
+	context = get_blog_list_common_data(request,blogs_all_list)
+	context['blog_type'] = blog_type
+	return render(request, 'blog/blogs_with_date.html', context)
 
-	def blog_with_date(request, year, month):
-		blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month) # 横线写法
-		context = get_blog_list_common_data(request, blog_all_list)
-		context['blogs_with_date'] = '%s年%s月'%(year, month)
-		return render(request, 'blog/blogs_with_date.html', context)
+def blog_with_date(request, year, month):
+	blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month) # 横线写法
+	context = get_blog_list_common_data(request, blog_all_list)
+	context['blogs_with_date'] = '%s年%s月'%(year, month)
+	return render(request, 'blog/blogs_with_date.html', context)
 
-	def blog_detail(request, blog_pk):
-		blog = get_object_or_404(Blog, pk=blog_pk)
-		read_cookie_key = read_statistics_once_read(request, blog)
+def blog_detail(request, blog_pk):
+	''''''
+	blog = get_object_or_404(Blog, pk=blog_pk)
+	read_cookie_key = read_statistics_once_read(request, blog)
 
-		context = {}
-		context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
-		context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
-		context['blog'] = blog
-		response = render(request, 'blog/blog_detail.html', context)
-		response.set_cookie(read_cookie_key, 'true') # set_cookie写法
-		return response
+	context = {}
+	context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
+	context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
+	context['blog'] = blog
+	response = render(request, 'blog/blog_detail.html', context)
+	response.set_cookie(read_cookie_key, 'true') # set_cookie写法
+	return response
