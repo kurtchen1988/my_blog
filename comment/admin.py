6f973038877c07699350d0a3c4bea74f4b80e983
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-10-29 00:03:11 +0800

    2018-10-29

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	'''注册评论模块'''
+	list_display = ('id','content_object', 'text','comment_time','user','root','parent')
--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
+from django.http import JsonResponse
+from .models import Comment
+from .models import CommentForm
 
 # Create your views here.
+def update_comment(request):
+	referer = request.META.get('HTTP_REFERER', reverse('home'))
+	commnet_form = CommentForm(request.POST, user=request.user)
+	data = {}
+
+	if comment_form.is_valid():
+		# 检查通过，保存数据
+		comment = Comment()
