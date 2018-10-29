fc234e17108143a08ea96c6b26456308e7c1bdf1
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-10-30 00:02:31 +0800

    2018-10-30

--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -13,3 +13,35 @@ def read_statistics_once_read(request, obj):
 		readnum, created = ReadNum.objects.get_or_create(content_type=ct, object_id=obj.pk)
 		readnum.read_num += 1
 		readnum.save()
+
+		# 当天阅读数 +1
+		date = timezone.now().date()
+		readDetail, created = ReadDetail.objects.get_or_create(content_type=ct, object_id=obj.pk, date=date)
+		readDetail.read_num += 1
+		readDetail.save()
+
+	return key
+
+
+def get_seven_days_read_data(content_type):
+	today = timezone.now().date()
+	dates = []
+	read_nums = []
+	for i in range(7,0,-1):
+		date = today - datetime.timedelta(days=i)
+		dates.append(date.strftime('%m/%d'))
+		read_details = ReadDetail.objects.filter(content_type=content_type, date=today)
+		result = read_details.aggregate(read_num_sum=Sum('read_num'))
+		read_num.append(result['read_num_sum'] or 0)
+	return dates, read_nums
+
+def get_today_hot_date(content_type):
+	today = timezone.now().date()
+	read_details = ReadDetail.objects.filter(content_type=content_type, date=today).order_by('-read_num')
+	return read_details[:7]
+
+def get_yesterday_hot_data(content_type):
+	today = timezone.now.date()
+	yesterday = today - datetime.timedelta(days=1)
+	read_details = ReadDetail.objects.filter(content_type=content_type, date=yesterday).order_by('-read_num')
+	return read_details[:7]
