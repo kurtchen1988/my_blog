abc7002e654362a32bca1c699a9cf98866654bc1
bantu <76251098@qq.com>
2018-11-05 20:27:30 +0800

    2018-11-5

--- comment/forms.py
+++ comment/forms.py
@@ -3,22 +3,27 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models import ObjectDoesNotExist
 from ckeditor.widgets import CKEditorWidget
 from .models import Comment
-
+# https://docs.djangoproject.com/en/2.1/topics/forms/
+# 上面的文章要好好读，写了所有关于表单的基本介绍
 class CommentForm(forms.Form):
 	content_type = forms.CharField(widget=forms.HiddenInput)
+	# 内容类型，是字符串类型，在表单中为隐藏项
 	object_id = forms.IntegerField(widget=forms.HiddenInput)
+	# 项目类型，是整数型，在表单中为隐藏项
 	text = forms.CharField(widget=CKEditorWidget(config_name = 'comment_ckeditor'), error_messages={'required':'评论内容不能为空'})
+	# 回复内容，是字符串类型，在表单中是CKEditorWidget，错误信息为需要填入信息
 	reply_comment_id = forms.IntegerField(widget=forms.HiddenInput(attrs={'id':'reply_comment_id'}))
-
+	# 回复id，是整数型，在表单中为隐藏类型
 	def __init__(self, *args, **kwargs):
 		if 'user' in kwargs:
 			self.user = kwargs.pop('user')
 		super(CommentForm, self).__init__(*args, **kwargs)
 
 	def clean(self):
-		# 判断用户是否登录
+		# 判断用户是否登录，如果没登陆报一个验证错
 		if self.user.is_authenticated:
-			self.cleaned_data['user'] = self.user
+			self.cleaned_data['user'] = self.user # https://docs.djangoproject.com/en/2.1/ref/forms/validation/
+			# 这里的cleaned_data是django的验证步骤
 
 		else:
 			raise forms.ValidationError('用户尚未登录')
@@ -37,6 +42,7 @@ class CommentForm(forms.Form):
 		return self.cleaned_data
 
 	def clean_reply_comment_id(self):
+		'''清除回复的id，当回复id为0意思是它为文章的第一个留言，小于0是错的，当它回复的id存在就拿到它的信息，否则报错'''
 		reply_comment_id = self.cleaned_data['reply_comment_id']
 		if reply_comment_id < 0:
 			raise forms.ValidationError('回复出错')
--- comment/templatetags/comment_tags.py
+++ comment/templatetags/comment_tags.py
@@ -7,17 +7,20 @@ register = template.Library()
 
 @register.simple_tag
 def get_comment_count(obj):
+	'''注册自定义标签，返回评论个数'''
 	content_type = ContentType.objects.get_for_model(obj)
 	return Comment.objects.filter(content_type=content_type, object_id=obj.pk).count()
 
 @register.simple_tag
 def get_comment_form(obj):
+	'''返回表单，并注册自定义标签'''
 	content_type = ContentType.objects.get_for_model(obj)
 	form = CommentForm(initial={'content_type':content_type.model,'object_id':obj.pk,'reply_comment_id':0})
 	return form
 
 @register.simple_tag
 def get_comment_list(obj):
+	'''返回评论的结果集，并以评论时间倒序排列'''
 	content_type = ContentType.objects.get_for_model(obj)
 	comments = Comment.objects.filter(content_type=content_type, object_id=obj.pk, parent=None)
 	return comments.order_by('-comment_time')
